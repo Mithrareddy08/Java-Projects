e5659ec1f44c4f1b5a608336fa3a8a6bbf66b8b0
Mithrareddy08 <sangamithravanga@gmail.com>
2023-06-03 20:33:42 -0500

    New assignment added

---
+++ Runtimeassignment/src/Picture.java
@@ -0,0 +1,16 @@
+public class Picture {
+    public void pictureColor() {
+        System.out.println("Color of the Picture");
+    }
+}
+    class Picture1 extends Picture{
+        public void pictureColor() {
+            System.out.println("Picture shows Black color ");
+        }
+    }
+    class Picture2 extends Picture{
+        public void pictureColor() {
+            System.out.println("Picture shows White color");
+        }
+    }
+
