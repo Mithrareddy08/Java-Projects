1267d5f7a3f0cbf80b45e6dac6573f2c44628127
Mithrareddy08 <sangamithravanga@gmail.com>
2023-06-06 12:35:47 -0500

    New Assignment added

--- Personage/src/Main.java
+++ Personage/src/Main.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Enter age:");
-        Scanner s = new Scanner(System.in);
-        int age = s.nextInt();
-        if(age > 12 && age < 19) {
+    static void Age(int age) {
+
+        if (age > 12 && age < 19) {
             System.out.println("teen");
         } else if (age < 13) {
             System.out.println("Kid");
-        }else if (age > 19) {
+        } else if (age > 19) {
             System.out.println("adult");
         }
+    }
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter age:");
+       int age = s.nextInt();
+        Age(age);
     }
 }
