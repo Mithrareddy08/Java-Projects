e3eb5e9c8a255c5f5366ef428e68aeaaa0297efc
Mithrareddy08 <sangamithravanga@gmail.com>
2023-06-16 12:07:21 -0500

    new assignment added

---
+++ CoreJava/src/week3/Main.java
@@ -0,0 +1,23 @@
+package week3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+      System.out.println("Enter a string");
+      Scanner sc = new Scanner(System.in);
+       String s1=sc.nextLine();
+       MyExcep n=new MyExcep();
+      n.Custom(s1);
+
+
+      //  printNum1 nt=new printNum1();
+      //  printNum nr=new printNum();
+      //  Thread t= new Thread(nr);
+      //  nt.start();
+     //   t.start();
+
+    }
+}
+
